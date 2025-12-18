e4e935e78172326e481a678e4da384922f7150e3
Simen Sørensen <simen.sorensen@haugnett.no>
2025-12-18 14:35:57 +0100

    Added aoc day 10 solution

---
+++ Advent-of-Code/2025/day10.py
@@ -0,0 +1,77 @@
+from collections import deque
+
+with open("input10.txt") as f:
+    inpl = [line.strip() for line in f]
+
+#Part 1
+arr = []
+buttons = []
+jolt = [] #Used in part 2
+
+for s in inpl:
+    curr = s.split()
+    arr.append(list(curr[0][1:-1]))
+    cl = []
+    for x in curr[1:-1]:
+        cl.append([int(a) for a in x[1:-1].split(",")])
+    buttons.append(cl)
+    jolt.append([int(a) for a in curr[-1][1:-1].split(",")])
+
+res = 0
+for i in range(len(arr)):
+    curr = ["."]*len(arr[i])
+    q = deque()
+    vis = {tuple(curr)}
+    q.append((curr,0))
+
+    while q:
+        cl,steps = q.popleft()
+        if cl == arr[i]:
+            res += steps
+            break
+        for but in buttons[i]:
+            newl = cl[:]
+            if isinstance(but,int):
+                newl[but] = "#" if newl[but] == "." else "."
+            else:
+                for ind in but:
+                    newl[ind] = "#" if newl[ind] == "." else "."
+            if tuple(newl) not in vis:
+                vis.add(tuple(newl))
+                q.append((newl,steps+1))
+print(f"Part 1: {res}")
+
+#Part 2 
+#Use Z3 to solve
+
+from z3 import * 
+
+res = 0
+for i in range(len(jolt)):
+    but = buttons[i]
+    t = jolt[i]
+    pl = []
+    indl = [[] for _ in range(len(t))]
+    z = Optimize()
+
+    for j in range(len(but)):
+        pl.append(Int(f"p_{j}")) #Z3 integer variable, number of times button j is pressed
+        z.add(pl[j]>=0) #For avoiding negative button presses
+
+    for j in range(len(but)):
+        for pos in but[j]:
+            indl[pos].append(j)
+
+    for k in range(len(indl)):
+        currsum = sum([pl[j] for j in indl[k]]) #Sum of button presses that count at pos k
+        z.add(currsum == t[k]) #The presses must be equal to target value at pos k to be valid
+
+    z.minimize(sum(pl)) #Minimize total num of presses
+    z.check()
+    model = z.model()
+
+    curr = 0
+    for x in pl:
+        curr += model[x].as_long()
+    res += curr
+print(f"Part 2: {res}")
