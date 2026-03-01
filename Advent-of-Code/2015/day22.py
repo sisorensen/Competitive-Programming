0d8a189a8111e12f010f8246b9f98c949ec51767
Simen Sørensen <simen.sorensen@haugnett.no>
2026-03-01 21:29:47 +0100

    solved day 22

---
+++ Advent-of-Code/2015/day22.py
@@ -0,0 +1,14 @@
+with open("input22.txt") as f:
+    l = [line.strip() for line in f]
+
+def part1():
+    hp,mp = 50,500
+    
+    return 0
+
+def part2():
+    
+    return 0
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
